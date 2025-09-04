8e1d5ee890e302230bb1cd39a62aa11d026fa8fa
Sasha <antonov.05068@gmail.com>
2025-09-04 14:35:46 +0300

    2

---
+++ src/2/check_parametr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ (!"$dirname" =~ ^[A-Za-z]+$) || (${#firstParam} -gt 7) ]]; then
+    echo "Ошибка!!! Неправильный ввод названия папки"
+    exit 1
+fi
+
+fileName="$(echo $namefile | awk -F. '{print $1}')"
+fileExt="$(echo $namefile | awk -F. '{print $2}')"
+if [[ ${#fileName} > 7 || ${#fileExt} > 3 ||\
+    $fileName =~ [^A-Za-z] || $fileExt =~ [^A-Za-z] ||\
+    ($fileExt == "") || ${#fileName} < 1 ]]; then
+    echo "Ошибка!!! Неправильный ввод названия файлов" 
+    exit 1
+fi
+
+filesize=$(echo $size | awk -F"Mb" '{print $1}')
+if [[ !($size =~ Mb$) || ($filesize =~ [^0-9]) || ($filesize -gt 100) || ($filesize -le 0) ]]; then
+    echo "Ошбка!!! Неправильный размер"
+    exit 1
+fi
+
+source ./add_file.sh
---
+++ src/2/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+START=$(date +%s%N)
+TIMES=$(date +%H:%M)
+
+if [[ $# -eq 3 ]]; then
+    dirname=$1
+    namefile=$2
+    size=$3
+    
+    source ./check_parametr.sh
+    END=$(date +%s%N)
+    DIFF=$((($END - $START)/1000000))
+    TIMEE=$(date +%H:%M)
+    echo "Время начала: $TIMES"
+    echo "Время конца: $TIMEE"
+    echo "Скрипт работает $DIFF ms"
+
+    echo "">>logFiles
+    echo "Start time: $TIMES" >>logFiles
+    echo "End time: $TIMEE" >>logFiles
+    echo "Script working $DIFF ms" >>logFiles
+else
+    echo "Ошибка, неправильные параметры"
+fi
