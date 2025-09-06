02c52ee0833d062009b5b32c90e9ab02746db805
Sasha <antonov.05068@gmail.com>
2025-09-06 14:03:19 +0300

    1234/

--- src/3/first.sh
+++ src/3/first.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo "Введите путь logfile"
-read log
-remfiles=$(cat ../02/"$log"| awk -F'|' '{print $2}')
-if [ -e ../02/"$log" ]; then 
-    echo "okey.Let's go"
-    for delete in $remfiles
-    do
-    sudo rm -rf $delete
+echo "Введите путь к logfile (можно абсолютный или относительный)"
+read -r log
+
+if [ -e "$log" ]; then
+    echo "okey. Let's go"
+    awk -F'|' '{print $2}' "$log" | while read -r delete; do
+        [ -n "$delete" ] && sudo rm -rf -- "$delete"
     done
-else 
-    echo "$log не найден"
+else
+    echo "Файл не найден: $log"
 fi
